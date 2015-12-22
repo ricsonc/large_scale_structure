a7a375376ee23ed544de373ed1b8d9879b8e8bdc
ricsoncheng <ricsonc@andrew.cmu.edu>
2015-12-22 12:31:53 -0500

    updated class

--- nbody.hpp
+++ nbody.hpp
@@ -1,25 +1,23 @@
 #ifndef NBODY
 #define NBODY
 
-struct NBody {
+class NBody {
+    public:
         std::vector<Body> bodies;
         UniverseArgs uargs;
         SimulationArgs sargs;
+        void simulate(verbose = True);
+        void draw();
+    private:
         Tree *quadtree;
-        
         void init_field();
         void metric_expansion();
         void border_wrap();
         void build_qtree();
         void leapfrog();
-        //what do these two do?
-        Vec accel(Vec p1, Vec p2, Real mass, Real distance);
-        Vec lattice_accel(Vec pos, RVec center, Real lattice_dist);
-        Vec body_accel()
-        std::vector<Vec> accelerations();
         void step();
-        void simulate(verbose = True);
-        void draw();
+        Vec accel_body(Body B);
+        std::vector<Vec> accelerations();
 };
 
 #endif
